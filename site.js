096d928f82aa36e281cc74266a39bfef495018cf
nebulous108 <shreyanshsuccess@gmail.com>
2026-09-13 13:13:06 +1000

    Delete site.js

--- site.js
+++
@@ -1,2 +0,0 @@
-
-document.addEventListener('DOMContentLoaded',()=>{const b=document.getElementById('menuBtn'),m=document.getElementById('mobileMenu');if(b&&m)b.addEventListener('click',()=>m.classList.toggle('open'));});
